45fa2a5bd7e5de9bf6685b8e82fc8b5e3c006f65
zdodev <iver0945@gmail.com>
2021-01-05 00:35:48 +0900

    Queue 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Queue.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct Queue<T> {
+    private var data = [T]()
+    
+    mutating func enqueue(element: T) {
+        return data.append(element)
+    }
+    
+    mutating func dequeue() -> T? {
+        if data.isEmpty {
+            return nil
+        } else {
+            return data.removeFirst()
+        }
+    }
+    
+    mutating func peek() -> T? {
+        return data.first
+    }
+    
+    mutating func isEmpty() -> Bool {
+        return data.isEmpty
+    }
+}
