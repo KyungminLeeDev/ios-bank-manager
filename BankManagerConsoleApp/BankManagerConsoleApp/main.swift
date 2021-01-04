80d0f90c1071159ca94d7ede0baba08106a51db2
zdodev <iver0945@gmail.com>
2021-01-05 01:26:49 +0900

    feat: 은행 개점

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -5,3 +5,21 @@
 // 
 
 import Foundation
+
+while true {
+    print("1: 은행 개점")
+    print("2: 종료")
+    print("입력: ", terminator: "")
+    
+    let choice = readLine()
+    if choice == "1" {
+        var banker = BankManager()
+        banker.open(banker: 1)
+        banker.greetingCustomer(number: Int.random(in: 10...30))
+        banker.process()
+    } else if choice == "2" {
+        break
+    } else {
+        print("잘못 입력하셨습니다. 다시 입력해주세요.")
+    }
+}
