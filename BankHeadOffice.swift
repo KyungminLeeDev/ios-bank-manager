2fbba92658498028e73f45c3865c3d86b3c8a6c7
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-01-14 01:28:52 +0900

    Add: BankHeadOffice 싱글턴 클래스 파일 추가

---
+++ BankHeadOffice.swift
@@ -0,0 +1,14 @@
+//
+//  BankHeadOffice.swift
+//  BankManagerConsoleApp
+//
+//  Created by Kyungmin Lee on 2021/01/14.
+//
+
+import Foundation
+
+class BankHeadOffice {
+    static let shared: BankHeadOffice = BankHeadOffice()
+    
+    private init() {}
+}
