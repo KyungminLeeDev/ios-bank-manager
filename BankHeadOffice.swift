7c3203c2c206f2fa7e5aed1c292e060e7ec5e03b
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-01-14 04:17:50 +0900

    Feat: 대출 심사 기능 추가
    - 대출 심사 전용 디스패치큐 추가
    - 대출 심사

--- BankHeadOffice.swift
+++ BankHeadOffice.swift
@@ -9,6 +9,25 @@ import Foundation
 
 class BankHeadOffice {
     static let shared: BankHeadOffice = BankHeadOffice()
+    private let judgementLoanQueue: DispatchQueue = DispatchQueue(label: "JudgementLoan")
+    private let judgeLoanTime: TimeInterval = 0.5
+    
+    func addJudgementLoan(_ bankClerk: BankClerk) {
+        judgementLoanQueue.sync {
+            judgeLoan(bankClerk)
+        }
+    }
+    
+    func judgeLoan(_ bankClerk: BankClerk) {
+        guard let client = bankClerk.currentClient else {
+            return
+        }
+                
+        print("\(client.tag)번 \(client.priority.string)고객 대출심사 시작")
+        Thread.sleep(forTimeInterval: judgeLoanTime)
+        print("\(client.tag)번 \(client.priority.string)고객 대출심사 완료")
+        bankClerk.executeLoan()
+    }
     
     private init() {}
 }
